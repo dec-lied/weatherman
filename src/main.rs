f85905359c96de0b81fd524009641b4b174ac9c1
dec-lied <gavindhondt@hotmail.com>
2023-01-12 14:04:23 -0500

    progress on ui

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ pub mod weather;
 use tui::
 {
     backend::CrosstermBackend,
-    widgets::Paragraph,
+    widgets::{Paragraph, ListState, List},
     layout::Rect,
     Terminal
 };
@@ -13,7 +13,7 @@ use std::
     time::{Instant, Duration},
     sync::mpsc, thread
 };
-use ui::{ForecastPosition, Screen};
+use ui::{forecast_screen::ForecastPosition, Screen};
 use crossterm::event::KeyCode;
 use weather::WeeklyForecast;
 use std::io;
@@ -30,9 +30,9 @@ async fn main()
     (
         move |rect|
         {
-            let loading_panel: Rect = ui::get_loading_panel(rect.size());
+            let loading_panel: Rect = ui::loading_screen::get_loading_panel(rect.size());
 
-            let loading_message: Paragraph = ui::get_loading_message();
+            let loading_message: Paragraph = ui::loading_screen::get_loading_message();
 
             rect.render_widget(loading_message, loading_panel);
         }
@@ -72,14 +72,18 @@ async fn main()
     );
 
     // setting up main loop
-    let mut screen: Screen = Screen::HOME;
+    let mut screen: Screen = Screen::FORECAST;
+    
+    // initializing menu state
+    let mut menu_state: ListState = ListState::default();
+    menu_state.select(Some(0));
 
     loop
     {
         // checking for screen
         match screen
         {
-            Screen::HOME =>
+            Screen::FORECAST =>
             {
                 // drawing home screen
                 terminal.draw
@@ -87,25 +91,23 @@ async fn main()
                     |rect|
                     {
                         // drawing simple elements
-                        let main_panel: Vec<Rect> = ui::get_home_panel(rect.size());
+                        let main_panel: Vec<Rect> = ui::forecast_screen::get_forecast_panel(rect.size());
 
-                        let title: Paragraph = ui::get_title();
-                        let tabs: Paragraph = ui::get_tabs();
-                        let controls: Paragraph = ui::get_controls();
+                        let title: Paragraph = ui::forecast_screen::get_forecast_title();
+                        let controls: Paragraph = ui::forecast_screen::get_forecast_controls();
 
                         rect.render_widget(title, main_panel[0]);
-                        rect.render_widget(tabs, main_panel[1]);
-                        rect.render_widget(controls, main_panel[3]);
+                        rect.render_widget(controls, main_panel[2]);
 
                         // drawing forecast
-                        let forecast_panel: Vec<Rect> = ui::get_forecast_panel(main_panel[2]);
+                        let forecast_panel: Vec<Rect> = ui::forecast_screen::get_forecast_slot_panel(main_panel[1]);
                         let forecast_slots: Vec< Vec<Rect> > = weekly_forecast.days.iter()
                             .enumerate()
-                            .map(|(i, _)| ui::get_forecast_slot_layout(forecast_panel[i]))
+                            .map(|(i, _)| ui::forecast_screen::get_forecast_slot_layout(forecast_panel[i]))
                             .collect();
 
                         let daily_paragraph_sets: Vec< Vec<Paragraph> > = weekly_forecast.days.iter().enumerate()
-                            .map(|(index, day)| ui::get_forecast_paragraphs(day, ForecastPosition::from_index(&index)))
+                            .map(|(index, day)| ui::forecast_screen::get_forecast_paragraphs(day, ForecastPosition::from_index(&index)))
                             .collect();
 
                         for paragraph_set in daily_paragraph_sets.into_iter().zip(forecast_slots.into_iter())
@@ -123,20 +125,119 @@ async fn main()
                 {
                     crossterm::event::Event::Key(key) => match key.code
                     {
-                        KeyCode::Char('q') =>
-                        {
-                            break;
-                        },
-                        KeyCode::Char('m') =>
-                        {
-                            screen = Screen::MENU;
-                        }
+                        KeyCode::Char('q') => break,
+                        KeyCode::Char('m') => screen = Screen::MENU,
                         _ => {}
                     },
                     _ => {}
                 }
             },
-            _ => {}
+            Screen::MENU =>
+            {
+                terminal.draw
+                (
+                    |rect|
+                    {
+                        let menu_panel: Vec<Rect> = ui::menu_screen::get_menu_panel(rect.size());
+
+                        let menu_list: List = ui::menu_screen::get_menu_list();
+
+                        rect.render_stateful_widget(menu_list, menu_panel[1], &mut menu_state);
+                    }
+                ).expect("failed to draw on terminal");
+
+                match rx.recv().expect("failed to read from mpsc")
+                {
+                    crossterm::event::Event::Key(key) => match key.code
+                    {
+                        KeyCode::Char('q') => break,
+                        KeyCode::Char('j') =>
+                        {
+                            if let Some(sel_index) = menu_state.selected()
+                            {
+                                menu_state.select(Some(((sel_index) + 1) % 3));
+                            }
+                            else
+                            {
+                                menu_state.select(Some(0));
+                            }
+                        },
+                        KeyCode::Char('k') =>
+                        {
+                            if let Some(sel_index) = menu_state.selected()
+                            {
+                                if sel_index == 0
+                                {
+                                    menu_state.select(Some(2));
+                                }
+                                else
+                                {
+                                    menu_state.select(Some(sel_index - 1));
+                                }
+                            }
+                            else
+                            {
+                                menu_state.select(Some(2));
+                            }
+                        },
+                        KeyCode::Enter =>
+                        {
+                            if let Some(sel_index) = menu_state.selected()
+                            {
+                                screen = match sel_index
+                                {
+                                    0 => Screen::FORECAST,
+                                    1 => Screen::HOURLY,
+                                    2 => Screen::OPTIONS,
+                                    _ => Screen::FORECAST
+                                };
+                            }
+                        },
+                        _ => {}
+                    },
+                    _ => {}
+                }
+            },
+            Screen::HOURLY =>
+            {
+                terminal.draw
+                (
+                    |rect|
+                    {
+                        
+                    }
+                ).expect("failed to draw on terminal");
+
+                match rx.recv().expect("failed to read from mpsc")
+                {
+                    crossterm::event::Event::Key(key) => match key.code
+                    {
+                        KeyCode::Char('q') => break,
+                        _ => {}
+                    },
+                    _ => {}
+                }
+            },
+            Screen::OPTIONS =>
+            {
+                terminal.draw
+                (
+                    |rect|
+                    {
+
+                    }
+                ).expect("failed to draw on terminal");
+
+                match rx.recv().expect("failed to read from mpsc")
+                {
+                    crossterm::event::Event::Key(key) => match key.code
+                    {
+                        KeyCode::Char('q') => break,
+                        _ => {}
+                    },
+                    _ => {}
+                }
+            }
         }
     }
 
